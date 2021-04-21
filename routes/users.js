cf9f520f207eb9df9ea0bfcf850962a69ad74485
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-04-21 09:11:37 +0300

    Refactored routes

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const passport = require('passport');
 const users = require('../controllers/users')
 const catchAsync = require('../utils/catchAsync');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout)
 
