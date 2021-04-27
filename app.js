60a2c933e0b1a1182d3b670c04fef6162d84ca86
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-04-27 12:59:49 +0300

    Ready for deploying

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
