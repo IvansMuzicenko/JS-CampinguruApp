3d3e4ddde8b0eeafc4d63cc8d51c65907ad7af23
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-04-27 12:45:41 +0300

    Ready for deploying

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userRoutes = require('./routes/users');
 
 const app = express();
 
-const MongoDBStore = require('connect-mongo')(session);
+const MongoDBStore = require('connect-mongo');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/campinguru';
 
