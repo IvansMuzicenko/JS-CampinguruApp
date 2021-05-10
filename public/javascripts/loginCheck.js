dcb5ab8693e6dc654e85b61c2bf444e5b33b064b
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-05-10 14:31:46 +0300

    Login api

--- public/javascripts/loginCheck.js
+++ public/javascripts/loginCheck.js
@@ -4,8 +4,7 @@ const loginForm = document.getElementById('loginForm');
 
 const userInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
-const username = userInput.value;
-const password = passwordInput.value;
+
 const invalidFeedback = document.getElementById('invalidFeedback');
 alertA.classList.add('none');
 invalidFeedback.innerHTML = 'Incorrect login or password';
@@ -14,3 +13,34 @@ userInput.classList.remove('is-valid', 'is-invalid');
 passwordInput.classList.remove('is-valid', 'is-invalid');
 userInput.classList.add('is-invalid');
 passwordInput.classList.add('is-invalid');
+
+const validate = (e) => {
+	e.preventDefault();
+
+	const username = userInput.value;
+	const password = passwordInput.value;
+	const body = { username, password };
+
+	fetch('/login', {
+		method: 'POST',
+		body: JSON.stringify(body),
+		headers: { 'Content-type': 'application/json; charset=UTF-8' }
+	})
+		.then(function (response) {
+			return response.text();
+		})
+		.then(function (html) {
+			let parser = new DOMParser();
+			let doc = parser.parseFromString(html, 'text/html');
+
+			let user = doc.getElementById('user');
+			if (user) {
+				loginForm.submit();
+			}
+		})
+		.catch(function (err) {
+			console.warn('Something went wrong.', err);
+		});
+};
+
+loginForm.addEventListener('submit', validate);
