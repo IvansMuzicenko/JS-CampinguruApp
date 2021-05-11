3792b5f9d525791accd12a253428c349883b4011
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-05-11 12:32:02 +0300

    Cloudinary path change

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
 	cloudinary,
 	params: {
-		folder: 'campinguru',
+		folder: `campinguru/campgrounds/`,
 		allowedFormats: ['jpeg', 'png', 'jpg']
 	}
 });
