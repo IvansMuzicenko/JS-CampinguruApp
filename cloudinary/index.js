ba15c249c45398046c991513d3ef1d4d63c0e568
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-05-12 11:44:43 +0300

    Tinify test

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,6 +1,10 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+const fs = require('fs');
+const tinify = require('tinify');
+tinify.key = process.env.TINIFY_KEY;
+
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_KEY,
@@ -11,6 +15,24 @@ const storage = new CloudinaryStorage({
 	cloudinary,
 	params: {
 		folder: (req, file) => {
+			// const files = Object.entries(file);
+			// // console.log(files);
+			// // console.log(files[1][1]);
+			// const buff = Buffer.from(files, 'base64');
+			// console.log(buff);
+
+			// tinify.fromBuffer(buff).toBuffer(function (err, resultData) {
+			// 	if (err) throw err;
+			// 	console.log(resultData);
+			// });
+			// fs.readFile(files[1][1], function (err, sourceData) {
+			// 	if (err) throw err;
+			// 	tinify.fromBuffer(sourceData).toBuffer(function (err, resultData) {
+			// 		if (err) throw err;
+			// 		// ...
+			// 	});
+			// });
+
 			const titleFull = req.body.campground.title;
 			const title = titleFull.replace(/ /g, '-');
 			return `campinguru/campgrounds/${title}`;
